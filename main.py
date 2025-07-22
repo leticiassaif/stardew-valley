e6eb85b7f7e6addcd41ad680726479101ee5da7f
leticiassaif <leticiassaif@gmail.com>
2025-07-22 13:07:12 -0300

    small changes

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame, sys
+from settings import *
+
+class Game:
+    def __innit__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Garden")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            dt = self.clock.tick() / 1000
+            pygame.display.update() 
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+
+# #fonte
+# font = pygame.font.Font(None, 50) #font type, font size -- font/*nome*.ttf
+# screen.blit(test_surface,(200,100)) #surface, position
+# screen.blit(text_surface,(300,50))
+
+
+# clock.tick(60) # 60fps no máximo
+# # else running = False
+
+# #surfaces
+# test_surface = pygame.Surface((100,200))
+# # para imagem : pygame.image.load(pasta/nome da imagem)
+# test_surface.fill("red")
+# text_surface = font.render("My game", False, "Green") #text, AA, color
---
+++ settings.py
@@ -0,0 +1,52 @@
+from pygame.math import Vector2
+# screen
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+TILE_SIZE = 64
+
+# overlay positions
+overlay_pos = {
+    "tool" : (40, SCREEN_HEIGHT - 15),
+    "seed" : (70, SCREEN_HEIGHT - 5)}
+
+player_tool_offset = {
+    "left" : Vector2(-50,40),
+    "right" : Vector2(50,40),
+    "up" : Vector2(0,-10),
+    "down" : Vector2(0,50)
+}
+
+layers = {
+    "water" : 0,
+    "ground" : 1,
+    "soil" : 2,
+    "soil water" : 3,
+    "rain floor" : 4,
+    "house bottom" : 5,
+    "ground plant" : 6,
+    "main" : 7,
+    "house top" : 8,
+    "fruit" : 9,
+    "rain drops" : 10
+}
+
+apple_pos = {
+    "small" : [(18,17), (30,37), (12,50), (30,45), (20,30), (30,10)],
+    "large" : [(30,24), (60,65), (50,50), (16,40), (45,50), (42,70)]
+}
+
+grow_speed = {
+    "corn" : 1,
+    "tomato" : 0.7
+}
+
+sale_prices = {
+    "wood" : 4,
+    "apple" : 2,
+    "corn" : 10,
+    "tomato" : 20
+}
+
+seed_prices = {
+    "corn" : 4,
+    "tomato" : 5,
+}
