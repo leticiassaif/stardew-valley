efb59db1133960c0f049083608870815b34be1a0
leticiassaif <leticiassaif@gmail.com>
2025-07-22 14:15:40 -0300

    player changes

--- player.py
+++ player.py
@@ -5,6 +5,30 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((64,32)) #w, h
+        # general setup
+        self.image = pygame.Surface((32,64)) #w, h
         self.image.fill("green")
         self.rect = self.image.get_rect(center = pos)
+
+        # movement attributes
+        self.direction = pygame.math.Vector2()
+        # self.pos = pygame.math.Vector2()
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self, dt):
+        self.input()
