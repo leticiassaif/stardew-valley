6b323c678a9109e18ddc9319260f7761d24ca74c
leticiassaif <leticiassaif@gmail.com>
2025-07-24 11:08:16 -0300

    tool aparecendo na tela

--- code/overlay.py
+++ code/overlay.py
@@ -12,5 +12,11 @@ class Overlay:
         overlay_path = "./graphics/overlay/"
         self.tools_surf = {tool: pygame.image.load(f"{overlay_path}{tool}.png").convert_alpha() for tool in player.tools}
         self.seeds_surf = {seed: pygame.image.load(f"{overlay_path}{seed}.png").convert_alpha() for seed in player.seeds}
-        print(self.tools_surf)
-        print(self.seeds_surf)
+    
+    def display(self):
+
+        # tools
+        tool_surf = self.tools_surf[self.player.selected_tool]
+        self.display_surface.blit(tool_surf(0,0)) #x, y
+
+        # seeds
