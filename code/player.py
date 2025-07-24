b098b6538e9da209caa1e7cd53f866d0f3659de3
leticiassaif <leticiassaif@gmail.com>
2025-07-24 10:15:40 -0300

    switching between tools

--- code/player.py
+++ code/player.py
@@ -22,14 +22,18 @@ class Player(pygame.sprite.Sprite):
 
         # timers
         self.timers = {
-            "tool use": Timer(350, self.use_tool)
+            "tool use": Timer(350, self.use_tool),
+            "tool switch": Timer(200)
         }
 
         # tools
-        self.selected_tool = "axe"
+        self.tools = ["hoe", "axe", "water"]
+        self.tool_index = 0
+        self.selected_tool = self.tools[self.tool_index]
     
     def use_tool(self):
-        print(self.selected_tool)
+        pass
+        # print(self.selected_tool)
 
     def import_assets(self):
         self.animations = {"up": [],"down": [],"left": [],"right": [],
@@ -77,6 +81,13 @@ class Player(pygame.sprite.Sprite):
                 self.timers["tool use"].activate()
                 self.direction = pygame.math.Vector2()
                 self.frame_index = 0
+            
+            # change tool
+            if keys[pygame.K_q] and not self.timers["tool switch"].active:
+                self.timers["tool switch"].activate()
+                self.tool_index += 1
+                self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
+                self.selected_tool = self.tools[self.tool_index]
 
     def get_status(self):
         # idle status
