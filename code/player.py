fa98a7a260e003ff0d8c62c3ae28b4a4ced72fc9
leticiassaif <leticiassaif@gmail.com>
2025-07-24 23:48:01 -0300

    parte de inventário

--- code/player.py
+++ code/player.py
@@ -44,6 +44,13 @@ class Player(pygame.sprite.Sprite):
         self.seed_index = 0
         self.selected_seed = self.seeds[self.seed_index]
 
+        self.item_inventory = {
+            "wood" :   0,
+            "apple" :  0,
+            "corn" :   0,
+            "tomato" : 0
+        }
+
         #Interaction
         self.tree_sprites = tree_sprites
 
