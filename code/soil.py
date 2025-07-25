647bde537fa5466186c7a37fc1e17b76fc21935c
leticiassaif <leticiassaif@gmail.com>
2025-07-25 10:16:42 -0300

    testee

--- code/soil.py
+++ code/soil.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from pytmx.util_pygame import load_pygame
 
 class SoilLayer:
     def __init__(self, all_sprites):
@@ -21,4 +22,6 @@ class SoilLayer:
         h_tiles, v_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
         
         self.grid = [ [[] for col in range(h_tiles)] for row in range(v_tiles) ]
+        for x, y, _ in load_pygame("./data/map.tmx").get_layer_by_name("Farmable").tiles():
+            self.grid[y][x].append("F")
         print(self.grid)
